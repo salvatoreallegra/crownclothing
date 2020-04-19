cad6daa7c3b785c85918bf8599b3dec4bf7c4e2c
Salvatore Allegra <salallegra@gmail.com>
2020-04-18 23:37:14 -0700

    Install node-sass package and add .scss file with styles

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./homepage.styles.scss";
 
 const HomePage = () => {
   return (
@@ -7,31 +8,31 @@ const HomePage = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Hats</h1>
-            <span className="sub-title">Shop Now!</span>
+            <span className="subtitle">Shop Now!</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Jackets</h1>
-            <span className="sub-title">Shop Now!</span>
+            <span className="subtitle">Shop Now!</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Sweaters</h1>
-            <span className="sub-title">Shop Now!</span>
+            <span className="subtitle">Shop Now!</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Womens</h1>
-            <span className="sub-title">Shop Now!</span>
+            <span className="subtitle">Shop Now!</span>
           </div>
         </div>
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Mens</h1>
-            <span className="sub-title">Shop Now!</span>
+            <span className="subtitle">Shop Now!</span>
           </div>
         </div>
       </div>
