afa392361a559a50c80c706bc02dbef8c5797c76
Salvatore Allegra <salallegra@gmail.com>
2020-04-18 23:27:48 -0700

    Create initial homepage component with dummy markup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import HomePage from "./homepage.component";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
